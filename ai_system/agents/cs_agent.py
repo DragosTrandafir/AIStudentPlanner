a5b940cfae54aac867a7bfe900845957b61818e1
DragosTrandafiri <dragostrandafir333@gmail.com>
2025-11-04 10:11:06 +0200

    agents structure

--- ai_system/agents/cs_agent.py
+++ ai_system/agents/cs_agent.py
@@ -4,7 +4,7 @@ from ai_system.agents.base_agent import BaseAgent
 
 class CSAgent(BaseAgent):
     def propose_plan(self, subject_data):
-        prompt=""
+        prompt=" g"
         response = self.model.get_response(prompt)
         return json.load(response)
 
