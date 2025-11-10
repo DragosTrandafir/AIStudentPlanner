b78ae191a60299ea46abdb71e59567662c6828bf
DragosTrandafiri <dragostrandafir333@gmail.com>
2025-11-10 17:14:59 +0200

    last modifications

--- ai_system/orchestrator/ai_orchestrator.py
+++ ai_system/orchestrator/ai_orchestrator.py
@@ -19,7 +19,7 @@ custom_agent_model = os.getenv("CUSTOM_AGENT_MODEL")
 # response = cs_agent1.propose_agent_plan("C:/Users/DragosTrandafiri/PycharmProjects/AlphaFlow/ai_system/agents"
 #                                         "/mock_data_cs.json")
 # print(response)
-
+#
 cs_agent2 = MathAgent(hf_token, custom_agent_model)
 response = cs_agent2.propose_agent_plan("C:/Users/DragosTrandafiri/PycharmProjects/AlphaFlow/ai_system/agents"
                                         "/mock_data_math.json")
